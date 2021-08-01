9ae25d23debd7195c5fe9b29c4fea86237576fd4
sakumar199789 <vskumarsanku88@gmail.com>
2021-08-01 16:34:51 +0530

    Heroku Config

--- src/com/capgemini/asset/test/AssetTest.java
+++ src/com/capgemini/asset/test/AssetTest.java
@@ -1,13 +1,9 @@
 package com.capgemini.asset.test;
-import static org.junit.Assert.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 import com.capgemini.asset.bean.AssetBean;
 import com.capgemini.asset.bean.UserBean;
@@ -16,63 +12,7 @@ import com.capgemini.asset.dao.IAssetdao;
 import com.capgemini.asset.exception.AssetException;
 
 public class AssetTest {
-	Connection connection;
-	IAssetdao iAsset=new Assetdaoimpl();
-	@BeforeClass
-	public static void initialize() {
-		IAssetdao iAsset=new Assetdaoimpl();
-	
-	}
-	
-	
-	@Test
-	public void test() {
-		UserBean user=new UserBean();
-		user.setUserNameId("Sai");
-		user.setPassword("Sai@1997");
 		
-		try {
-			assertEquals(true,iAsset.dataAuthentication(user));
-		} catch (AssetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	
-	
-	}
-	
-	@Test
-	public void test2() {
-		UserBean user=new UserBean();
-		user.setUserNameId("Dinesh");
-		user.setPassword("Dines@123");
-		
-		try {
-			assertEquals(true,iAsset.dataAuthentication(user));
-		} catch (AssetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-}
-	
-
-	@Test
-	public void addNewAssetTest() {
-		AssetBean asset=new AssetBean();
-		asset.setAssetId(14567);
-		asset.setAssetName("Pendrive");
-		asset.setAssetDes("Storage media");
-		asset.setQuantity(20);
-		asset.setStatus("AVAILABLE");
-		try{
-		assertEquals(1,iAsset.addNewAsset(asset));
-		}
-	
-		catch(AssetException a){
-			System.out.println(a);
-		}
-		}
-	
 	
 
 	/*@AfterClass
--- src/com/capgemini/asset/util/DBConnection.java
+++ src/com/capgemini/asset/util/DBConnection.java
@@ -4,6 +4,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,31 +14,46 @@ import java.util.Properties;
 import com.capgemini.asset.exception.*;
 
 public class DBConnection {
-	
-	private static Properties properties= new Properties();
+
+	private static Properties properties = new Properties();
 	private static Connection connection;
 
-	public static Connection getConnection() throws AssetException{
-		
+	/*
+	 * public static Connection getConnection() throws AssetException{
+	 * 
+	 * try { InputStream inputStream =new
+	 * FileInputStream("resources/jdbc.properties"); properties.load(inputStream);
+	 * String url=properties.getProperty("dburl"); String
+	 * user=properties.getProperty("username"); String
+	 * password=properties.getProperty("password"); inputStream.close();
+	 * //DriverManager.registerDriver(new oracle.jdbc.OracleDriver()); connection =
+	 * DriverManager.getConnection("jdbc:oracle:thin:@10.219.34.3:1521/orcl",
+	 * "trg1109", "training1109");
+	 * 
+	 * } catch (SQLException e) { throw new AssetException(e.getMessage()); } catch
+	 * (FileNotFoundException e2) { throw new
+	 * AssetException("Could not Find properties file to connect to database."); }
+	 * catch (IOException e) { throw new
+	 * AssetException("Could not read the database details from properties file.");
+	 * return connection;
+	 * 
+	 * }
+	 */
+
+	public static Connection getConnection() throws AssetException {
 		try {
-			/*InputStream inputStream =new FileInputStream("resources/jdbc.properties");
-			properties.load(inputStream);
-			String url=properties.getProperty("dburl");
-			String user=properties.getProperty("username");
-			String password=properties.getProperty("password");
-			inputStream.close();*/
-			DriverManager.registerDriver(new oracle.jdbc.OracleDriver()); 
-			connection = DriverManager.getConnection("jdbc:oracle:thin:@10.219.34.3:1521/orcl", "trg1109", "training1109");
-			
+			URI dbUri = new URI("postgres://xnmisrdmlrriig:efe9b205e0244f3992329f5ac820d16e88bf7ae1a8e10d528024c715406cbb56@ec2-52-0-114-209.compute-1.amazonaws.com:5432/d7fbgntvhngr41");
+
+		    String username = dbUri.getUserInfo().split(":")[0];
+		    String password = dbUri.getUserInfo().split(":")[1];
+		    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+		    return DriverManager.getConnection(dbUrl, username, password);
 		} catch (SQLException e) {
 			throw new AssetException(e.getMessage());
-		}
-		/* catch (FileNotFoundException e2) {
-		throw new AssetException("Could not Find properties file to connect to database.");
-	} catch (IOException e) {
-		throw new AssetException("Could not read the database details from properties file.");*/
-		return connection;
-	
+		}  catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			throw new AssetException(e.getMessage());
+		}		
 	}
 
-} 
+}
